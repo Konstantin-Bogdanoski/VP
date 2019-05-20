c259c3718af6fdb84ae4f5a3a2b0f2fc72a95960
Konstantin-Bogdanoski(konstantin.b@live.com) <konstantin.b@live.com>
2019-05-20 22:57:02 +0200

    - Add Serijalizacija Lab09

---
+++ Lab09/Serijalizacija/Serijalizacija/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serijalizacija
+{
+    [Serializable]
+    public class Ball
+    {
+        public int Radius { get; set; }
+        public Color Color { get; set; }
+        public Point Location { get; set; }
+
+        public Ball(Color color)
+        {
+            this.Radius = 25;
+            this.Color = color;
+        }
+
+        public void Draw(Graphics g)
+        {
+            Brush brush = new SolidBrush(this.Color);
+        }
+        
+    }
+}
